bc0caa88fef4dc60f38fd451759508c2b8b1940d
Spr0y <terry110908.sdnwr@gmail.com>
2024-12-21 21:08:42 +0700

    Add files via upload

---
+++ gra_int.js
@@ -0,0 +1,9 @@
+const gameArea = document.getElementById('gameArea');
+
+function startGame() {
+    // Game initialization code here
+    gameArea.innerHTML = "<p>The game has started!</p>";
+    // You can add more game logic below
+}
+
+startGame();
